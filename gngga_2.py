af477c0cb8e3148641a2286190e5fefc0f610774
Subhadip Ghorui <49529825+subhadipghorui@users.noreply.github.com>
2019-12-18 21:05:16 +0530

    Getting lat lon alt

---
+++ gngga_2.py
@@ -0,0 +1,36 @@
+# Input TXT file path as string
+path = input("Enter File Path :")
+
+# Enter the file path
+myfile = open(path, "r")
+# print(myfile.read())
+
+# Creating a new file and closed it
+newFile = open("lat_lon_alt.csv", "w")
+newFile.writelines("lat, lon, alt" + "\n")
+newFile.close()
+# Set the cursor to the 0 position
+myfile.seek(0)
+alt: int = 0
+for db_set in myfile.readlines():
+    # fine the GNGGA data only
+    # Check the string position and store in a variable
+    val1 = db_set.find("$GNGGA")
+    if val1 > -1:
+        print(db_set)
+        print(val1)
+
+        data = db_set
+        lat = data[(val1+18): (val1+28)]
+        lon = data[(val1+31): (val1+42)]
+        
+        alt1 = float(data[(val1+55): (val1+59)])
+        alt2 = float(data[(val1+62): (val1+67)])
+        alt = alt1 + alt2
+
+        print(lat, lon, alt)
+
+        # Write to a new file
+        newFile = open("lat_lon_alt.csv", "a")
+        newFile.writelines(str(lat) + ", " + str(lon) + ", " + str(alt) + "\n")
+        newFile.close()
